5dbf29323d42907b17acf17bdae98829ea5e4b8a
Jaffarserver93 <159849165+Jaffarserver93@users.noreply.github.com>
2025-06-27 09:21:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// #update this line 👇
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['openai'], // Explicitly externalize openai to avoid bundling issues
-    },
-  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  }
 });
